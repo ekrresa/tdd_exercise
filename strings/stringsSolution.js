79bc6a96bf099f8413d874c495efada7f3aaf35f
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-05 18:11:20 +0100

    Pass test when no arguments are passed

--- strings/stringsSolution.js
+++ strings/stringsSolution.js
@@ -1,5 +1,8 @@
 var joinStrings = function(first, last) {
-  return first + last;
+  if (typeof first === "string" && typeof last === "string") {
+    return first + last;
+  }
+  throw "only strings are valid and required";
 };
 
 module.exports = joinStrings;
--- strings/stringsSolution.test.js
+++ strings/stringsSolution.test.js
@@ -4,4 +4,7 @@ describe("String Concatenation", function() {
   test("should add two strings", function() {
     expect(joinStrings("lamb", "stew")).toBe("lambstew");
   });
+  test("should throw exception if no argument is passed", function() {
+    expect(() => joinStrings()).toThrow("only strings are valid and required");
+  });
 });
