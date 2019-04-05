d22bc08e5e88f4de97338e08d70b0a9e7b5eba9f
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-04-05 10:27:35 +0100

    Write tests to add two numbered strings

--- add_two_num/add.test.js
+++ add_two_num/add.test.js
@@ -7,3 +7,19 @@ test("should add two numbers", function () {
 test("should add two positive numbers", function () {
   expect(add(23,17)).toBe(40)
 })
+
+test("should add two negative numbers", function () {
+  expect(add(-23,-17)).toBe(-40)
+})
+
+test("should add a positive and a negative number", function () {
+  expect(add(-23,32)).toBe(9)
+})
+
+test("should add a number and a string", function () {
+  expect(add("-23",32)).toBe(9)
+})
+
+test("should add two numbered strings", function () {
+  expect(add("-23","6")).toBe(-17)
+})
